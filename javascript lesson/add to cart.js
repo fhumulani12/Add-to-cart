4b16439e754792c05d266afb34f42ac42a6bdb39
fhumulani mulaudzi <115710782+fhumulani12@users.noreply.github.com>
2023-01-29 11:45:06 +0200

    Add files via upload

---
+++ javascript lesson/add to cart.js
@@ -0,0 +1,17 @@
+function btn256f(){
+   document.getElementById("price").innerHTML = "R26 699";
+}
+
+function btn128f(){
+    document.getElementById("price").innerHTML = "R26 099";
+ }
+ 
+
+ function btn512f(){
+    document.getElementById("price").innerHTML = "R33 999";
+ }
+ 
+ function btnTB(){
+    document.getElementById("price").innerHTML = "R39 199";
+ }
+ 
